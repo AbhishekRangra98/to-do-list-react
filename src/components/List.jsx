80d45807091794686bdf3ec37a9c7d03a7887ab9
abhishek <abhishekrangra270898@gmail.com>
2026-06-22 13:24:02 +0530

    Updated project

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -14,6 +14,6 @@ export default function List({ val, setVal, addHandler, isEdit, error }) {
       </form>
 
       {error && <div className="error-msg">{error}</div>}
-    </>
-  )
+      </>
+      )
 }
